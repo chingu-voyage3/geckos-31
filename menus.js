0a880d5d7628d84c28bb400d5e886592754f27fb
CatSoundsAwesome <wigfridwilson@gmail.com>
2017-12-30 17:31:38 -0800

    adds minus sign menu and toggle function

--- menus.js
+++ menus.js
@@ -1,15 +1,20 @@
 // Event listener on cog icon - opens/closes settings menu
 var plusSign = document.getElementById("plusSign");
+var minusSign = document.getElementById("minusSign");
 var cog = document.getElementById("cog");
 var plusSignMenu = document.querySelector(".plusSignMenu");
+var minusSignMenu = document.querySelector(".minusSignMenu");
 var settingsMenu = document.querySelector(".settingsMenu");
 
 
 // Menu Event Listeners
 plusSign.addEventListener('click', function() {
-	toggleHideMenu(plusSignMenu)
+	toggleHideMenu(plusSignMenu);
 });
 
+minusSign.addEventListener('click', function() {
+	toggleHideMenu(minusSignMenu);
+})
 
 cog.addEventListener('click', function() {
 	toggleHideMenu(settingsMenu);
